8553b129e6075525c34dc6a98aa45a082a6f3d4c
Berat BAYRAM <berat.bayram@tubitak.gov.tr>
2025-03-21 17:23:30 +0300

    Implement cat fetching functionality with modern async/await syntax

---
+++ app/fetch/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { FormEvent, useEffect, useState } from "react";
+
+// import styles from "./page.module.css";
+
+async function fetchCatNew() {
+  const response = await fetch("https://cataas.com/cat");
+  const blob = await response.blob();
+  return URL.createObjectURL(blob);
+}
+
+function fetchCatOld() {
+  return fetch("https://cataas.com/cat")
+    .then((res) => res.blob())
+    .then((blob) => URL.createObjectURL(blob));
+}
+
+export default function Home() {
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    fetchCatNew().then((url) => setUrl(url));
+    // fetchCatOld().then((url) => setUrl(url));
+  }, []);
+
+  /*
+  useEffect(() => {
+    fetch("https://cataas.com/cat")
+      .then((res) => res.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        setUrl(url);
+      });
+  }, []);
+  */
+
+  return (
+    <main>
+      <h1>Random Cat</h1>
+      {url ? <img src={url} alt="Random Cat" /> : <p>Loading...</p>}
+    </main>
+  );
+}
